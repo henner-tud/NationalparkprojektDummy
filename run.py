c72cc5a01e788e0c988e2457b9081a7e865df82a
henner <henner@17q.de>
2025-07-22 10:37:47 +0200

    first try

--- run.py
+++ run.py
@@ -30,10 +30,6 @@ def stop_processes():
     os.write(sys.stdout.fileno(), b"All processes stopped.\n")
     sys.exit(0)
 
-def signal_handler(sig, frame):
-    shutdown_event.set()
-    stop_processes()
-
 def wait_for_enter():
     try:
         input("Press Enter to exit...\n")
@@ -42,12 +38,6 @@ def wait_for_enter():
         stop_processes()
 
 if __name__ == "__main__":
-    # Register signal handlers
-    signal.signal(signal.SIGINT, signal_handler)    # Ctrl+C
-    signal.signal(signal.SIGTERM, signal_handler)   # kill
-    if sys.platform != "win32":
-        signal.signal(signal.SIGHUP, signal_handler)
-
     print("\n" + "="*50)
     print("National Park Project Dummy")
     print("="*50 + "\n")
